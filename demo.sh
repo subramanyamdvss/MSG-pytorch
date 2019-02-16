17e30c517bf0014780d541242a9c79d47d08c77a
subramanyamdvss <subramanyamdvss@gmail.com>
2019-02-16 22:59:50 +0530

    after training

--- demo.sh
+++ demo.sh
@@ -1,6 +1,6 @@
 set -e
 
-python main.py --method l12rmsg --nepochs 20 --log_interval 1
-python main.py --method incremental --nepochs 20 --log_interval 1
-python main.py --method sgd --nepochs 20 --log_interval 1
-python main.py --method original  
+python -u main.py --method l12rmsg --nepochs 20 --log_interval 1
+python -u main.py --method incremental --nepochs 20 --log_interval 1
+python -u main.py --method sgd --nepochs 20 --log_interval 1
+python -u main.py --method original  
